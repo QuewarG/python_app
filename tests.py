001235dfaf49bc8031c913e44d99c9d7dd634eff
Kevin Girón <kevin.giron@correounivalle.edu.co>
2023-10-26 22:46:25 -0500

    Feature#0-TestDeConexión2

--- tests.py
+++ tests.py
@@ -2,5 +2,5 @@ from hello import app
 
 with app.test_client() as c:
     response = c.get('/')
-    assert response.data == b'<p>Hello World2!</p>'
-    assert response.status_code == 402
+    assert response.data == b'<p>Hello World!</p>'
+    assert response.status_code == 200
